cfe54ab7639dfe37723ff561333857e5a5b63441
William-Torrance <60004854+William-Torrance@users.noreply.github.com>
2020-03-03 08:41:39 -0500

    Update SavingsAccount.cpp

--- Part-2/SavingsAccount.cpp
+++ Part-2/SavingsAccount.cpp
@@ -5,13 +5,15 @@ using namespace std;
 // constructor initializes balance and interest rate
 SavingsAccount::SavingsAccount( double initialBalance, double rate ) : Account( initialBalance ) 
 {
-	// your code
+    interestRate = rate; // percentage
 }
 
 double SavingsAccount::calculateInterest() {
-	// your code
+    return Account::getBalance() * interestRate;
 } 
 void SavingsAccount::display(ostream & os) const
 {
-	// your code
+    cout << "Account type: Saving" << endl;
+    cout << "Balance: $" << Account::getBalance() << endl;
+    cout << "Interest Rate (%): " << interestRate << endl;
 }
